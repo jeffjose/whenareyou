36866ef26a7b620106206bd162a382f973c82470
Florian Obersteiner <florian.obersteiner@kit.edu>
2021-07-05 17:54:11 +0200

    bugfix function name

--- whenareyou/__init__.py
+++ whenareyou/__init__.py
@@ -1,5 +1,5 @@
-from whenareyou.whenareyou import whenareyou, whenareyou_apt
+from whenareyou.whenareyou import whenareyou, whenareyou_IATA
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
 # determines which objects will be imported with "import *"
-__all__ = ("whenareyou", "whenareyou_apt")
+__all__ = ("whenareyou", "whenareyou_IATA")
