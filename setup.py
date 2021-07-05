46776d7bc99e40a2b3e3467e4b7948bea58921aa
Florian Obersteiner <florian.obersteiner@kit.edu>
2021-07-05 16:50:37 +0200

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open('README.rst') as readme:
 
 setup(
     name='whenareyou',
-    version='0.3.4',
+    version='0.3.5',
     description='Gets the time zone of any location in the world',
     long_description=long_description,
     url='https://github.com/aerupt/whenareyou',
