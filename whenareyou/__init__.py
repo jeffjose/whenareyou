fcfc6fd5d0168485cddcaadb9b644321f4ae925e
Florian Obersteiner <f.obersteiner@kit.edu>
2022-03-07 21:10:22 +0100

    obtain version from package metadata

--- whenareyou/__init__.py
+++ whenareyou/__init__.py
@@ -1,6 +1,21 @@
 from whenareyou.whenareyou import whenareyou, whenareyou_IATA
-from whenareyou._version import __version__
 
 # https://docs.python.org/3/tutorial/modules.html#importing-from-a-package
 # determines which objects will be imported with "import *"
 __all__ = ("whenareyou", "whenareyou_IATA")
+
+
+def get_version():
+    try:
+        # Py 3.8+
+        from importlib import metadata
+
+        return metadata.version("imktk")
+    except ImportError:
+        # Py <= 3.7
+        import pkg_resources
+
+        return pkg_resources.get_distribution("imktk").version
+
+
+__version__ = get_version()
